f68cd24712367e10ec7ec58cc21d5f0a6fa7295d
tinkerjoe-git <nicholas.stagno@gmail.com>
2021-06-11 19:21:29 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,41 @@
+
 export default (state = [], action) => {
-  return state;
+  
+
+  let idx;
+  let quoteIndex;
+  let quote;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+    return state.filter(quote => quote.id !== action.quoteId);
+    case "UPVOTE_QUOTE":
+     quoteIndex = state.findIndex(function(quote) {
+      return quote.id === action.quoteId
+      })
+      quote = state[quoteIndex]
+      return [
+        ...state.slice(0, quoteIndex),
+        Object.assign({},quote,{votes: quote.votes += 1}),
+        ...state.slice(quoteIndex+1)
+      ]
+
+    case "DOWNVOTE_QUOTE":
+    quoteIndex = state.findIndex(function(quote) {
+     return quote.id === action.quoteId
+     })
+     quote = state[quoteIndex]
+           if (quote.votes > 0) {
+     return [
+       ...state.slice(0, quoteIndex),
+       Object.assign({},quote,{votes: quote.votes -= 1}),
+       ...state.slice(quoteIndex+1)
+     ]}
+     return state;
+
+
+    default:
+      return state;
+  }
 }
